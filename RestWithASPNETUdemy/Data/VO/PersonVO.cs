ebed80b1271bdb05492da4e886baba5f2c352ee1
Pablo Victor Simões de Lima <pablosdlima@hotmail.com>
2021-12-23 07:36:32 -0300

    Custom Serialization

--- RestWithASPNETUdemy/Data/VO/PersonVO.cs
+++ RestWithASPNETUdemy/Data/VO/PersonVO.cs
@@ -1,11 +1,22 @@
-﻿namespace RestWithASPNETUdemy.Data.VO
+﻿using System.Text.Json.Serialization;
+
+namespace RestWithASPNETUdemy.Data.VO
 {
     public class PersonVO
     {
+        [JsonPropertyName("Code")] //"data annotations" de api
         public long Id { get; set; }
+
+        [JsonPropertyName("Name")]
         public string FirstName { get; set; }
+
+        [JsonPropertyName("Last")]
         public string LastName { get; set; }
+
+        [JsonIgnore]
         public string Address { get; set; }
+
+        [JsonPropertyName("Sex")]
         public string Gender { get; set; }
     }
 }
